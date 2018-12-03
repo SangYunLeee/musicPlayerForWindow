eee543866998d99bd7d81866e2850ad9678b2916
sororiri <sororiri@gmail.com>
2018-12-03 23:55:35 +0900

    0.1v

---
+++ musicPlayer/main.cpp
@@ -0,0 +1,27 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QStringList>
+#include <QDebug>
+#include "music.h"
+#include "musiclist.h"
+
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+
+    engine.rootContext()->setContextProperty("CppMusicList", new MusicList());
+
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
